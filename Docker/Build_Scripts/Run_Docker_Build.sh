a042bb45f485f747a216f301d79cb761e933518d
mxlei01 <mxlei01@gmail.com>
2016-09-26 18:23:45 -0600

    Change docker and requirements for nose tests

--- Docker/Build_Scripts/Run_Docker_Build.sh
+++ Docker/Build_Scripts/Run_Docker_Build.sh
@@ -4,5 +4,4 @@ cd /application/Application/ && (../Build_Scripts/Constant_Message.sh &)
 
 # Run tests, and send results to coveralls.io
 # COVERALLS_REPO_TOKEN=$COVERALLS_REPO_TOKEN TRAVIS_BRANCH=$TRAVIS_BRANCH BRANCH=$TRAVIS_BRANCH coveralls
-coverage3 run --rcfile=/application/.coveragerc -m unit_tests.run_all_tests && 
-COVERALLS_REPO_TOKEN=$1 TRAVIS_BRANCH=$2 BRANCH=$3 coveralls
+nosetests -c /application/.noserc unit_tests && COVERALLS_REPO_TOKEN=$1 TRAVIS_BRANCH=$2 BRANCH=$3 coveralls
