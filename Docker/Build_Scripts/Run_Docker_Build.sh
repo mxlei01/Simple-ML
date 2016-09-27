8a604d1f49a75be930bb1fbe9684cc53eb4d395c
mxlei01 <mxlei01@gmail.com>
2016-09-26 21:15:07 -0600

    Add nosetests options

--- Docker/Build_Scripts/Run_Docker_Build.sh
+++ Docker/Build_Scripts/Run_Docker_Build.sh
@@ -4,4 +4,4 @@ cd /application/Application/ && (../Build_Scripts/Constant_Message.sh &)
 
 # Run tests, and send results to coveralls.io
 # COVERALLS_REPO_TOKEN=$COVERALLS_REPO_TOKEN TRAVIS_BRANCH=$TRAVIS_BRANCH BRANCH=$TRAVIS_BRANCH coveralls
-cd /application/Application/ && chmod -R -x * && nosetests -c ../.noserc --with-coverage unit_tests && COVERALLS_REPO_TOKEN=$1 TRAVIS_BRANCH=$2 BRANCH=$3 coveralls --rcfile=../.coveragerc
+cd /application/Application/ && chmod -R -x * && nosetests -c ../.noserc --with-coverage --cover-inclusive --cover-tests --cover-package=. unit_tests && COVERALLS_REPO_TOKEN=$1 TRAVIS_BRANCH=$2 BRANCH=$3 coveralls --rcfile=../.coveragerc
