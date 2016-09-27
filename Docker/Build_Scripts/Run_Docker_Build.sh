f927c2beddbe191e866ddc916769885d72032cc8
mxlei01 <mxlei01@gmail.com>
2016-09-26 19:55:50 -0600

    Using nosetests

--- Docker/Build_Scripts/Run_Docker_Build.sh
+++ Docker/Build_Scripts/Run_Docker_Build.sh
@@ -4,4 +4,4 @@ cd /application/Application/ && (../Build_Scripts/Constant_Message.sh &)
 
 # Run tests, and send results to coveralls.io
 # COVERALLS_REPO_TOKEN=$COVERALLS_REPO_TOKEN TRAVIS_BRANCH=$TRAVIS_BRANCH BRANCH=$TRAVIS_BRANCH coveralls
-nosetests -c .noserc unit_tests && COVERALLS_REPO_TOKEN=$1 TRAVIS_BRANCH=$2 BRANCH=$3 coveralls
+cd /application/Application/ && chmod -R -x * && nosetests -c ../.noserc --with-coverage unit_tests && COVERALLS_REPO_TOKEN=$1 TRAVIS_BRANCH=$2 BRANCH=$3 coveralls
