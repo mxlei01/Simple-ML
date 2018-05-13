0749d22a3f83fdfd5a3954abe4af25277cb7921d
mxlei01 <Fjo-45s-5zx-6vG-Dfg>
2018-05-12 23:19:32 -0600

    Added comments to implementation for linear.py

--- simpleml/machine_learning/neural_networks/linear.py
+++ simpleml/machine_learning/neural_networks/linear.py
@@ -62,8 +62,8 @@ class Linear(Node):
             grad_cost = n.gradients[self]
 
             # inputs[0]=x, partial of the loss respective to this node's input.
-            # Derivative:  dl    d
-            #              -- = --∑w_i*x_i+b=w_i
+            # Derivative:  dl   d
+            #              -- = --(∑w_i*x_i+b)=w_i
             #              dx   dx
             # self.outputs[1].value.T = w.T = 10x13
             #        dc  ds
@@ -73,13 +73,63 @@ class Linear(Node):
             # dc   dc  ds  dnet
             # -- = -- ---- ----, where s is the sigmoid function
             # dx   ds dnet  dx
+            #                  s_1               net_1
+            #            |-------------|        |-----|
+            #                   1            e^(w_1*x_1)
+            #    = -(y - --------------)(-------------------)(  w_1  )
+            #            1+e^-(w_1*x_1)  (1 + e^(w_1*x_1))^2  
+            #      |-------------------||-------------------||-------|    
+            #                dc                  ds_1         dnet_1
+            #               ----                ------        ------
+            #               ds_1                dnet_1         dx_1
             # 2x13 (gradients for x) = 2x10 (grad_cost) * 10x13 (w.T), if the inputs are from training data, then
             # we will do nothing with the gradients, however, if the gradients are another layer of activation with
             # weights, then we can pass the cost downwards.
             self.gradients[self.inputs[0]] += np.dot(grad_cost, self.outputs[1].value.T)
 
             # inputs[1]=w, partial of the loss respective to this node's output
+            # Derivative: dl   d
+            #             -- = --(∑w_i*x_i+b)=x_i
+            #             dw   dw
+            # self.outputs[0].value.T = x.T = 13x2
+            #             dc  ds
+            # np.dot(x.T, -- ----(grad_cost)) = 13x10, this 13x10 produces the cost for the weights of 13x10
+            #             ds dnet
+            # Cost with respective to W for the network:
+            # dc   dc  ds  dnet
+            # -- = -- ---- ----, where s is the sigmoid function
+            # dw   ds dnet  dw
+            #                  s_1               net_1
+            #            |-------------|        |-----|
+            #                   1            e^(w_1*x_1)
+            #    = -(y - --------------)(-------------------)(  w_1  )
+            #            1+e^-(w_1*x_1)  (1 + e^(w_1*x_1))^2  
+            #      |-------------------||-------------------||-------|    
+            #                dc                  ds_1         dnet_1
+            #               ----                ------        ------
+            #               ds_1                dnet_1         dx_1
+            # 13x10 (gradients for w) = 13x2 (x.T) * 2*10 (grad_cost), this is the gradient values for w, and we can
+            # use the gradient values to update w.
             self.gradients[self.inputs[1]] += np.dot(self.outputs[0].value.T, grad_cost)
 
             # inputs[2]=b, partial of the loss respective to this node's bias
+            # Derivative: dl   d
+            #             -- = --(∑w_i*x_i+b)=1
+            #             db   dw
+            # np.sum(grad_cost) = 1x10 matrix
+            # Cost with respective to b for the network:
+            # dc   dc  ds  dnet
+            # -- = -- ---- ----, where s is the sigmoid function
+            # db   ds dnet  db
+            #                  s_1               net_1
+            #            |-------------|        |-----|
+            #                   1            e^(w_1*x_1)
+            #    = -(y - --------------)(-------------------)(1)
+            #            1+e^-(w_1*x_1)  (1 + e^(w_1*x_1))^2
+            #      |-------------------||-------------------||-------|
+            #                dc                  ds_1         dnet_1
+            #               ----                ------        ------
+            #               ds_1                dnet_1          db
+            # 1x10 (gradients for b) = 2x10 with columns summed together, this is the gradient values for b, and we can
+            # use the gradient values to update b.
             self.gradients[self.inputs[2]] += np.sum(grad_cost, axis=0, keepdims=False)
